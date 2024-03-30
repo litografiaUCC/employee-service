78d5cd71d0b676a35da904c71420f650432efcc1
Cardona-JE <juancardonaa0421@gmail.com>
2024-03-29 20:56:01 -0500

    Add structure of the project

---
+++ src/main/java/com/litografiaartesplanchas/employeesservice/EmployeesserviceApplication.java
@@ -0,0 +1,13 @@
+package com.litografiaartesplanchas.employeesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesserviceApplication.class, args);
+	}
+
+}
